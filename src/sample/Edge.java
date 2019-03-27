3e5fcb560551a856fb7ea76eea330b7ef40f19e9
kigaru <jakubster9@gmail.com>
2019-03-27 11:35:19 +0000

    added get neighbors

--- src/sample/Edge.java
+++ src/sample/Edge.java
@@ -3,5 +3,17 @@ package sample;
 public class Edge {
     Node origin; //these names help if a route is unidirectional
     Node destination; //TODO junit test whether edges connect to node
-    int weight;
+    float weight;
+
+    public Node getOrigin() {
+        return origin;
+    }
+
+    public Node getDestination() {
+        return destination;
+    }
+
+    public float weight() {
+        return weight;
+    }
 }
--- src/sample/Node.java
+++ src/sample/Node.java
@@ -1,12 +1,21 @@
 package sample;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class Node {
     LinkedList<Edge> edges;
 
     public Node() {
         edges = new LinkedList<>();
+    }
+
+    public List<Node> getNeighbors() {
+        LinkedList<Node> neighbors = new LinkedList<Node>();
+        for(Edge e : edges) {
+            neighbors.add(e.getDestination());
+        }
+        return neighbors;
     }
 
     public void addEdge(Edge edge) {
